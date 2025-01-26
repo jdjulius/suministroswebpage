8cdcbfa646f4225498dfccb0a55bf9ffacd8be7d
jdjulius <julio.david.velasquez@gmail.com>
2025-01-25 21:14:49 -0600

    init

---
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-562.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-c405ce85', {"layout":"number","heading":"A\u00f1os de Experiencia","numberPrefix":"","numberSuffix":"%","startNumber":0,"endNumber":98,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-5d03f64c', {"layout":"number","heading":"Clientes Atendidos","numberPrefix":"","numberSuffix":"+","startNumber":0,"endNumber":200,"totalNumber":200,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-803805b1', {"layout":"number","heading":"Lorem Ipsum","numberPrefix":"","numberSuffix":"K","startNumber":0,"endNumber":12,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+ });
